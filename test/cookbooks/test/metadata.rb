50f8427f67536d61f896652e8b572b31bd0721d8
Taylor Monacelli <taylormonacelli@gmail.com>
2017-12-25 07:33:13 -0800

    add test

---
+++ test/cookbooks/test/metadata.rb
@@ -0,0 +1,10 @@
+name 'test'
+maintainer 'Taylor Monacelli'
+maintainer_email 'taylormonacelli@gmail.com'
+license 'All Rights Reserved'
+description 'Installs/Configures test'
+long_description 'Installs/Configures test'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'myworkstation'
