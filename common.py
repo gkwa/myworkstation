6134bc451e8850acd6428ee3d0041699a14a42a6
Taylor Monacelli <taylormonacelli@gmail.com>
2019-03-29 16:16:47 -0700

    remove unsued imports

--- common.py
+++ common.py
@@ -4,9 +4,9 @@ import tempfile
 import yaml
 
 try:
-    from yaml import CLoader as Loader, CDumper as Dumper
+    from yaml import CLoader as Loader
 except ImportError:
-    from yaml import Loader, Dumper
+    from yaml import Loader
 
 
 def load(path):
